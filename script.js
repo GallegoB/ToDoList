14e1a422f1d26d52a0af89de96b5751a34a417a1
Gallego Baptiste <bapgallego@gmail.com>
2024-11-14 18:33:59 +0100

    Create

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('connecté'); 
+const form = document.getElementById('form');
+console.log(form);
+const todo = document.getElementById('todo');
+console.log(todo);
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('bouton cliqué');
+    const todoValue = todo.value;
+    console.log(todoValue);
+})
