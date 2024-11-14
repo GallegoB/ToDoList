e4e3ae3a22cb4177347baef2c6ecfaa3f42b3d43
Gallego Baptiste <bapgallego@gmail.com>
2024-11-14 18:55:54 +0100

    Read

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ form.addEventListener('submit', function(e){
     console.log('bouton cliqué');
     const todoValue = todo.value;
     console.log(todoValue);
-})
+    // créé l'élément
+let item = `<div class="item">
+<p>${todoValue}</p>
+<button class="btn-delete">
+    <i class="fas fa-trash-alt"></i>
+</button>
+<button class="btn-archive">
+    <i class="fas fa-check-circle"></i>
+</button>`;
+
+const listItems = document.querySelector('.list-items');
+console.log(listItems);
+listItems.innerHTML += item;
+});
+
